4a5dbbbe4c670515d4443a0d3586cdb53f9698e0
MuyingLi <ml5333@nyu.edu>
2018-04-05 17:19:59 -0400

    HW4

--- Program.java
+++ Program.java
@@ -3,19 +3,19 @@ import java.util.*;
 import java.io.*;
 
 public class Program {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		String customerFileName = args[0];
-		
-		File customerFile = new File("customersfile.txt");
-		Scanner in;
+
 		try {
-			in = new Scanner(customerFile);
-		
-		String s = "";
-		while (in.hasNext()) {
-			s = s + "#" + in.nextLine();
-		}
-		System.out.println(s);
+			FileReader customerFile = new FileReader(customerFileName);
+			BufferedReader in = new BufferedReader(customerFile);
+
+
+			String s = "";
+			while (in.readLine()!=null) {
+				s = s + "#" + in.readLine();
+			}
+			System.out.println(s);
 		} catch (FileNotFoundException e) {
 			System.out.print("Invalid file name.");
 		}
